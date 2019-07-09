5dfb3329098c09fbc53fdca1d25c695970221ea5
ssafydj <lee.seunghyun0343@gmail.com>
2019-07-09 15:47:15 +0900

    add .gitignore

---
+++ 00_startcamp/02_day/f_string.py
@@ -0,0 +1,23 @@
+name = '이승현'
+
+print(f'안녕하세요, {name} 입니다.')
+
+# 점심메뉴추천
+import random
+
+menu = ['짜장', '짬뽕', '탕수육']
+
+lunch = random.choice(menu)
+
+print(f'오늘의 점심은 {lunch}입니다.')
+
+# 로또 추천
+
+numbers = range(1, 46)
+lotto = random.sample(numbers, 6)
+
+print(f'오늘의 로또 당첨번호는 {sorted(lotto)}입니다.')
+
+# 필요하면 이렇게도 해보자
+name = '홍길동'
+print('안녕하세요, ' + name + '입니다')
---
+++ 00_startcamp/02_day/string_interpolation.py
@@ -0,0 +1,9 @@
+#f-string(new in 3.6)
+a = 'one'
+b = 'two'
+c = 'three' 
+
+print(f'{a}, {b}, {c}')
+
+
+
